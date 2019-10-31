6d1ead6014d610c2e140f4649b0a9dc4facb2a4e
MUedsa <7676275+MUedsa@users.noreply.github.com>
2019-10-31 13:44:44 +0800

    update:  get device status

--- BluetoothBatteryTest/Program.cs
+++ BluetoothBatteryTest/Program.cs
@@ -18,6 +18,44 @@ namespace BluetoothBatteryTest
             UInt32 Flags
         );
 
+        //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdienumdeviceinfo
+        [DllImport(@"Setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Boolean SetupDiEnumDeviceInfo(
+            IntPtr DeviceInfoSet,
+            Int32 MemberIndex,
+            ref SP_DEVINFO_DATA DeviceInfoData
+        );
+
+        //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdeviceregistrypropertyw
+        [DllImport(@"Setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Boolean SetupDiGetDeviceRegistryPropertyW(
+            IntPtr DeviceInfoSet,
+            ref SP_DEVINFO_DATA DeviceInfoData,
+            Int32 Property,
+            IntPtr PropertyRegDataType,
+            StringBuilder PropertyBuffer,
+            Int32 PropertyBufferSize,
+            ref Int32 RequiredSize
+        );
+
+        //https://docs.microsoft.com/zh-cn/windows-hardware/drivers/install/devpropkey
+        public struct DEVPROPKEY {
+            public Guid fmtid;
+            public UInt64 pid;
+        }
+
+        //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdevicepropertyw
+        [DllImport(@"Setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Boolean SetupDiGetDevicePropertyW(
+            IntPtr DeviceInfoSet,
+            ref SP_DEVINFO_DATA DeviceInfoData,
+            ref DEVPROPKEY PropertyKey,
+            ref UInt64 PropertyType,
+            [MarshalAs(UnmanagedType.LPArray)] byte[] PropertyBuffer,
+            Int32 PropertyBufferSize,
+            ref Int32 RequiredSize,
+            Int32 Flags
+        );
 
         //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdienumdeviceinterfaces
         [DllImport(@"Setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
@@ -29,7 +67,6 @@ namespace BluetoothBatteryTest
             ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData
         );
 
-
         //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdeviceinterfacedetailw
         [DllImport(@"Setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern Boolean SetupDiGetDeviceInterfaceDetailW(
@@ -40,7 +77,6 @@ namespace BluetoothBatteryTest
             ref UInt32 RequiredSize, 
             ref SP_DEVINFO_DATA DeviceInfoData
         );
-
 
         //https://docs.microsoft.com/en-us/windows/win32/api/setupapi/ns-setupapi-sp_devinfo_data
         //https://www.pinvoke.net/default.aspx/Structures/SP_DEVINFO_DATA.html
@@ -64,7 +100,6 @@ namespace BluetoothBatteryTest
             private UIntPtr reserved;
         }
 
-
         //https://docs.microsoft.com/zh-cn/windows/win32/api/setupapi/ns-setupapi-sp_device_interface_detail_data_w
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
         public struct SP_DEVICE_INTERFACE_DETAIL_DATA
@@ -73,6 +108,10 @@ namespace BluetoothBatteryTest
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)] public string DevicePath;
         }
 
+        public static readonly Int32 SPDRP_FRIENDLYNAME = 0x0000000C;
+        public static readonly Int32 SPDRP_DEVICEDESC = 0x00000000;
+        public static readonly Int32 SPDRP_ENUMERATOR_NAME = 0x00000016;
+        public static readonly Int32 SPDRP_HARDWAREID = 0x00000001;
 
         public static readonly UInt32 DIGCF_PRESENT = 0x02;
         public static readonly UInt32 DIGCF_DEVINTERFACE = 0x10;
@@ -91,6 +130,9 @@ namespace BluetoothBatteryTest
             [MarshalAs(UnmanagedType.U4)] FileAttributes flagsAndAttributes,
             IntPtr templateFile
         );
+
+        public static readonly int ERROR_INSUFFICIENT_BUFFER = 0x7A;
+        public static readonly int ERROR_INVALID_DATA = 0xD;
     }
 
     class BluetoothAPIs
@@ -179,7 +221,9 @@ namespace BluetoothBatteryTest
 
     class Program
     {
-        //GATT Service UUIDs 
+        //蓝牙协议 ID https://docs.microsoft.com/zh-cn/windows/uwp/devices-sensors/aep-service-class-ids
+        private static readonly string UUID = "{E0CBF06C-CD8B-4647-BB8A-263B43F0F974}";
+        //GATT Service UUID
         private static readonly string UUID1 = "{0000180F-0000-1000-8000-00805F9B34FB}"; //Battery Service
         private static readonly string UUID2 = "{00001800-0000-1000-8000-00805F9B34FB}"; //appearance
         private static readonly string UUID3 = "{0000180A-0000-1000-8000-00805F9B34FB}"; //DEVICE INFORMATION
@@ -192,12 +236,86 @@ namespace BluetoothBatteryTest
 
         static void Main(string[] args)
         {
+            GetDeviceNames(Guid.Parse(UUID));
             GetDevInfo(Guid.Parse(UUID1));
-            GetDevInfo(Guid.Parse(UUID2));
-            GetDevInfo(Guid.Parse(UUID3));
+            /*GetDevInfo(Guid.Parse(UUID2));
+            GetDevInfo(Guid.Parse(UUID3));*/
         }
 
-        static void GetDevInfo(Guid guid)
+        public static void GetDeviceNames(Guid guid)
+        {
+            IntPtr hdi = SetupAPI.SetupDiGetClassDevsW(ref guid, null, IntPtr.Zero, SetupAPI.DIGCF_PRESENT);
+            SetupAPI.SP_DEVINFO_DATA devInfoData = new SetupAPI.SP_DEVINFO_DATA();
+            devInfoData.cbSize = Marshal.SizeOf(devInfoData);
+            int i = 0;
+            while (SetupAPI.SetupDiEnumDeviceInfo(hdi, i, ref devInfoData))
+            {
+                i++;
+                String hwid = GetDeviceInfo(hdi, devInfoData, SetupAPI.SPDRP_HARDWAREID);
+                if (hwid.StartsWith("BTHLE\\Dev_"))
+                {
+                    String friendlyName = GetDeviceInfo(hdi, devInfoData, SetupAPI.SPDRP_FRIENDLYNAME);
+                    String deviceDesc = GetDeviceInfo(hdi, devInfoData, SetupAPI.SPDRP_DEVICEDESC);
+                    String enumeratorName = GetDeviceInfo(hdi, devInfoData, SetupAPI.SPDRP_ENUMERATOR_NAME);
+                    String status = GetConnectedStatus(hdi, devInfoData) ? "connected" : "paired";
+                    Console.WriteLine("hwid: {0}\nfriendlyName: {1}\ndeviceDesc: {2}\nenumeratorName: {3}\nstatus: {4}\n\n", hwid, friendlyName, deviceDesc, enumeratorName, status);
+                }
+            }
+        }
+
+        private static String GetDeviceInfo(IntPtr hdi, SetupAPI.SP_DEVINFO_DATA devInfoData, Int32 propertyName)
+        {
+            StringBuilder buffer = new StringBuilder();
+            Int32 bufferSize = 0;
+            while(!SetupAPI.SetupDiGetDeviceRegistryPropertyW(hdi, ref devInfoData, propertyName, IntPtr.Zero, buffer, bufferSize, ref bufferSize)){
+                int err = Marshal.GetLastWin32Error();
+                if(err == Kernel32.ERROR_INSUFFICIENT_BUFFER)
+                {
+                    buffer = new StringBuilder(bufferSize);
+                }
+                else if(err == Kernel32.ERROR_INVALID_DATA)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new Exception(err.ToString());
+                }
+            }
+            return buffer.ToString();
+        }
+         
+
+        private static Boolean GetConnectedStatus(IntPtr hdi, SetupAPI.SP_DEVINFO_DATA devInfoData)
+        {
+            Int32 DN_DEVICE_DISCONNECTED = 0x2000000;
+            UInt64 ulPropertyType = new UInt64();
+            Int32 dwSize = 4;
+            byte[] devst = new byte[4];
+            SetupAPI.DEVPROPKEY key = new SetupAPI.DEVPROPKEY();
+            key.fmtid = Guid.Parse("{4340a6c5-93fa-4706-972c-7b648008a5a7}");
+            key.pid = 2;
+            while (!SetupAPI.SetupDiGetDevicePropertyW(hdi, ref devInfoData, ref key, ref ulPropertyType, devst, dwSize, ref dwSize, 0))
+            {
+                int err = Marshal.GetLastWin32Error();
+                if (err == Kernel32.ERROR_INSUFFICIENT_BUFFER)
+                {
+                    devst = new byte[dwSize];
+                }
+                else if (err == Kernel32.ERROR_INVALID_DATA)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new Exception(err.ToString());
+                }
+            }
+            int status = BitConverter.ToInt32(devst);
+            return !Convert.ToBoolean(status & DN_DEVICE_DISCONNECTED);
+        }
+
+        public static void GetDevInfo(Guid guid)
         {
             IntPtr hdi = SetupAPI.SetupDiGetClassDevsW(ref guid, null, IntPtr.Zero, SetupAPI.DIGCF_PRESENT | SetupAPI.DIGCF_DEVINTERFACE);
             Console.WriteLine(hdi);
@@ -219,7 +337,7 @@ namespace BluetoothBatteryTest
             Console.WriteLine("\n\n");
         }
 
-        static void Communication(string path)
+        private static void Communication(string path)
         {
             IntPtr hDevice = Kernel32.CreateFileW(path, FileAccess.Read, FileShare.ReadWrite, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
             UInt16 servicesBufferCount = 0;
